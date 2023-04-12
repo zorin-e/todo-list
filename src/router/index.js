19a5ad930fc594d64fb328709f895b77e9bc1a13
Egor Zorin <zorinj@gmail.com>
2023-04-13 02:18:21 +0300

    fix(config): router base url

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/todo-list' : '/'),
   routes
 })
 
