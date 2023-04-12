1de917d144847391402d2b5b40cecdd6210d7914
Egor Zorin <zorinj@gmail.com>
2023-04-13 00:58:40 +0300

    feat(todos): add async routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TodoList from '@/views/TodoList.vue'
-import EditTodo from '@/components/Todo/TodoForm.vue'
+// import TodoList from '@/views/TodoList.vue'
+// import EditTodo from '@/components/Todo/TodoForm.vue'
+import { defineAsyncComponent } from 'vue'
+defineAsyncComponent
 
 const routes = [
   {
     path: '/',
     name: 'TodoList',
-    component: TodoList,
+    component: defineAsyncComponent(() => import('@/views/TodoList.vue')),
   },
   {
     path: '/add',
     name: 'AddTodo',
-    component: EditTodo,
+    component: defineAsyncComponent(() => import('@/components/Todo/TodoForm.vue')),
   },
   {
     path: '/edit/:id',
     name: 'EditTodo',
-    component: EditTodo,
+    component: defineAsyncComponent(() => import('@/components/Todo/TodoForm.vue')),
     props: route => ({ id: Number(route.params.id) }),
   }
 ]
